d4f5696eade263e0ec7df9f2b006ae3762a4a3c6
Redx-at <taoussi.aimen@gmail.com>
2025-07-10 00:44:31 +0100

    Bat commit

--- frontend/contact-book/app/Home.tsx
+++ frontend/contact-book/app/Home.tsx
@@ -1,10 +1,12 @@
 // App.jsx"
 "use client";
 import React, { useEffect, useRef, useState } from "react";
+import { FaUserLarge } from "react-icons/fa6";
 
 export default function App() {
   const [check, setCheck] = useState<string | null>(null);
   const [sizeLg, setSizeLg] = useState<number>(0);
+  const [modalActive, setModalActive] = useState<boolean>(false);
 
   // check the size with the window size
   useEffect(() => {
@@ -41,24 +43,48 @@ export default function App() {
     return;
   }, [check]);
   return (
-    <div className="h-screen w-full px-6 sm:px-10 lg:px-20 bg-overlay flex flex-col">
-      <div className="w-full h-[100px]  flex items-center justify-between">
+    <div className="reltive h-screen w-full px-6 sm:px-10 lg:px-20 bg-overlay flex flex-col">
+      <div className="w-full h-[100px]  flex items-center justify-between relative">
         <h1 className="font-bold text-2xl">eBazzar</h1>
-        <div className="flex items-center gap-4">
+        {sizeLg > 640 ? (
+          <div className="items-center gap-4 flex">
+            <button
+              className="transition-all duration-300 bg-white text-[#015B46] px-10 sm:px-12 py-2 cursor-pointer font-semibold text-[0.8rem] sm:text-sm rounded border border-gray-200 shadow-sm hover:bg-gray-100"
+              onClick={() => setCheck("login")}
+            >
+              Login
+            </button>
+            <button
+              className="transition-all duration-300 bg-[#015B46] text-white px-10 sm:px-12 py-2 cursor-pointer font-semibold text-[0.8rem] sm:text-sm rounded shadow-sm hover:bg-[#014b3c]"
+              onClick={() => setCheck("register")}
+            >
+              Sign Up
+            </button>
+          </div>
+        ) : (
           <button
-            className="transition-all duration-300 bg-white text-[#015B46] px-12 py-2 cursor-pointer font-semibold text-sm rounded border border-gray-200 shadow-sm hover:bg-gray-100"
-            onClick={() => setCheck("login")}
+            className="w-[40px] h-[30px] bg-[#015B46] rounded flex items-center justify-center cursor-pointer"
+            onClick={() => setModalActive(!modalActive)}
           >
-            Login
+            <FaUserLarge size={10} color="#FDF9F4" />
           </button>
-          <button
-            className="transition-all duration-300 bg-[#015B46] text-white px-12 py-2 cursor-pointer font-semibold text-sm rounded shadow-sm hover:bg-[#014b3c]"
-            onClick={() => setCheck("register")}
-          >
-            Sign Up
-          </button>
-        </div>
+          // <div className="items-center gap-4 flex">
+          //   <button
+          //     className="transition-all duration-300 bg-white text-[#015B46] px-10 sm:px-12 py-2 cursor-pointer font-semibold text-[0.8rem] sm:text-sm rounded border border-gray-200 shadow-sm hover:bg-gray-100"
+          //     onClick={() => setCheck("login")}
+          //   >
+          //     Login
+          //   </button>
+          //   <button
+          //     className="transition-all duration-300 bg-[#015B46] text-white px-10 sm:px-12 py-2 cursor-pointer font-semibold text-[0.8rem] sm:text-sm rounded shadow-sm hover:bg-[#014b3c]"
+          //     onClick={() => setCheck("register")}
+          //   >
+          //     Sign Up
+          //   </button>
+          // </div>
+        )}
       </div>
+
       <div className="flex items-center justify-between flex-1">
         {sizeLg > 1024 ? (
           <>
@@ -108,13 +134,13 @@ export default function App() {
             </p>
             <div className="flex items-center gap-4 mt-6 justify-center">
               <button
-                className="transition-all  duration-300 bg-[#015B46] text-white px-12 py-4 cursor-pointer font-semibold text-sm rounded shadow-sm hover:bg-[#014b3c] mt-4"
+                className="transition-all  duration-300 bg-[#015B46] text-white px-10 sm:px-12 py-3 sm:py-4 cursor-pointer font-semibold text-[0.8rem] sm:text-sm rounded shadow-sm hover:bg-[#014b3c] mt-4"
                 onClick={() => setCheck("register")}
               >
                 Get Started
               </button>
               <button
-                className="transition-all duration-300 bg-white text-[#015B46] px-12 py-4 border border-gray-200 cursor-pointer font-semibold text-sm rounded shadow-sm hover:bg-[#f1f1f1] mt-4"
+                className="transition-all duration-300 bg-white text-[#015B46] px-10 sm:px-12 py-3 sm:py-4 border border-gray-200 cursor-pointer font-semibold text-[0.8rem] sm:text-sm rounded shadow-sm hover:bg-[#f1f1f1] mt-4"
                 onClick={() => setCheck("register")}
               >
                 Explore Categorie
